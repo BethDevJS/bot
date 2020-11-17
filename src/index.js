072da3cd3118cbefdb5265fc59827f98e3357434
65502847 <qbotdev84@gmail.com>
2020-11-17 18:39:37 +0000

    Added the command handler (accessible under client as client.commands)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('../config.json');
+const { readdirSync } = require("fs")
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
   client.user.setPresence({
     activity: {name: 'https://github.com/good-discord-bot/bot', type: 'LISTENING'},
     status: config.status ? config.status : 'dnd'});
+  client.commands = new Discord.Collection();
+  const cmdFiles = readdirSync("./commands").filter(f => f.endsWith(".js"));
+  for (const file of cmdFiles) {
+    const command = require(`./commands/${file}`);
+    client.commands.set(command.name, command);
+    console.log(`${command.name} was loaded`)
+  };
 });
 
 
