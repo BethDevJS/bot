ed4cd88167dfef16ae4c485030e0490dbadc06ab
65502847 <qbotdev84@gmail.com>
2020-11-17 19:45:48 +0000

    Added the schema for a command so we are in sync (edit after asking for permisson)

---
+++ src/commands/command.schema.js
@@ -0,0 +1,11 @@
+/*
+    module.exports = {
+        name: "name",
+        aliases: ["al", "ia", "ses"],
+        help: "help",
+        usage: "usage",
+        async execute(client, message, args) {
+            code
+        }
+    }
+ */
