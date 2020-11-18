558a538175dc859910436d407815294b239055b7
Yuri <70341936+YxriDevOfficial@users.noreply.github.com>
2020-11-18 22:49:25 +0000

    Implimented debugging
    Add some debugging to find where the problem is, if it cannot find the command, can find it and isn't executing it or the event just isn't working overall.

--- src/events/message.js
+++ src/events/message.js
@@ -1,10 +1,13 @@
 module.exports = (client, msg) => {
   if (msg.author.bot || msg.content.startsWith(client.config.prefix)) return;
   const args = msg.content.slice(client.config.prefix.length).split(/ +/g);
+  // You said you couldn't get it to work so add some debugging
   try {
+    console.log("Command found")
     const command = client.commands.get(args[0]) || client.commands.find(c => c.aliases == args[0]);
     return command.execute(client, msg, args)
   } catch {
+    console.log("Command not found")
     return;
   }
 }
