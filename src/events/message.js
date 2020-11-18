1de4847118c33cac44aa9e89b5d740914294b80e
65502847 <qbotdev84@gmail.com>
2020-11-18 20:01:35 +0000

    h

--- src/events/message.js
+++ src/events/message.js
@@ -1,3 +1,10 @@
 module.exports = (client, msg) => {
   if (msg.author.bot || msg.content.startsWith(client.config.prefix)) return;
+  const args = msg.content.slice(client.config.prefix.length).split(/ +/g);
+  try {
+    const command = client.commands.get(args[0]) || client.commands.find(c => c.aliases == args[0]);
+    return command.execute(client, msg, args)
+  } catch {
+     return;
+  }
 }
